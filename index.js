4a7f884fb9b95824b1eb66b427d3209fc42f7b7a
Mostafa <126601729+mostafaDevadam@users.noreply.github.com>
2024-04-12 15:42:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
+
+app.use(express.json())
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
+})
+
+app.get('/api/v0/', (req, res) => {
+    res.json({msg: "start live api"})
 })
 app.listen(process.env.PORT || 3000)
